7e76c8cd7760869297685318c5697311c8dc3441
Divyarth <divyanshusaini@Dekshow.local>
2025-11-14 18:42:39 +0530

    Revert GitHub Pages config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/vaidya',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',

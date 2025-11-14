a4adb62b1d8ebb0e2004d9aa272a35d7ccab90a4
Divyarth <divyanshusaini@Dekshow.local>
2025-11-14 18:36:05 +0530

    Initial commit - React app

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
   output: 'export',
   basePath: '/vaidya',
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
